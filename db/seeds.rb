d6de036fac47cff85424abf89f983e316338524a
suhaibh <suhaib.habibullah@gmail.com>
2017-04-23 17:58:36 -0400

    Add status information to admin dash and post index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = User.create(email: "luke@example.com", password: "password", password_confirmation: "password",
+@user = User.create(email: "luke@example.com", password: "password", password_confirmation: "password",
 					first_name: "Luke", last_name: "Skywalker")
 
+puts "User created"
+
+@admin_user = AdminUser.create(email: "admin@example.com", password: "password", password_confirmation: "password",
+								first_name: "Admin", last_name: "User")
+
+puts "Admin User Created"
+
 50.times do |post|
-	Post.create(date: Date.today, rationale: "#{post + 1} rationale", user_id: user.id)
+	Post.create(date: Date.today, rationale: "#{post + 1} rationale", user_id: @user.id)
 end
 
 puts "100 posts created"
