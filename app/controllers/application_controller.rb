7eff3b5472b8af372067c730b2f9ba609c9f763d
suhaibh <suhaib.habibullah@gmail.com>
2017-07-30 14:56:20 -0400

    Add partial for growler alerts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,7 @@ class ApplicationController < ActionController::Base
 
   private
     def user_not_authorized
+      flash[:alert] = "You are not authorized to perform this action."
       redirect_to posts_path
     end
 end
