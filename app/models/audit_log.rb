8a1c55374a9da3fdeae12edc69a45d10c6b326dc
suhaibh <suhaib.habibullah@gmail.com>
2020-04-14 12:13:15 -0400

    Add update callback to set end date for audit logs

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -5,9 +5,15 @@ class AuditLog < ActiveRecord::Base
 
   after_initialize :set_defaults
 
+  before_update :set_end_date, if: :confirmed?
+
   paginates_per 10
 
   private
+  	def set_end_date
+  		self.end_date = Date.today
+  	end
+
   	def set_defaults
   		self.start_date ||= (Date.today - 6.days)
   	end
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,12 +23,12 @@ puts "User created"
 
 puts "Admin User Created"
 
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (1.week.ago - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (2.weeks.ago - 6.days))
+
 50.times do |post|
 	Post.create(date: Date.today, rationale: "#{post + 1} rationale content. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", user_id: @employee.id, overtime_request: 2.5)
 end
 
 puts "100 posts created"
-
-AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create(user_id: @employee.id, status: 0, start_date: (1.week.ago - 6.days))
-AuditLog.create(user_id: @employee.id, status: 0, start_date: (2.weeks.ago - 6.days))
